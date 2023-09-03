e7860bcb72df879726f5be1d0806393f6f79116f
Nassim <nassimsaboundji@gmail.com>
2023-09-02 22:04:22 -0400

    update

--- main.go
+++ main.go
@@ -3,7 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
+
+func getFiles(folderPath string) []string {
+	var files []string
+	err := filepath.Walk(folderPath, func (path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println("Error")
+	}
+
+	return files
+}
 
 func main() {
 	fmt.Println("Hello world!")
@@ -12,11 +34,5 @@ func main() {
 		fmt.Println(err)
 	}
 
-	files, err := os.Open("src")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(files)
+	fmt.Println(getFiles("./src"))
 }
